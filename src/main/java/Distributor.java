e6eac4ddb071eb6500ff8e2c9c61a2cd59535d80
jpkusch <jpkusch@umich.edu>
2020-03-27 01:10:06 -0400

    working Maven solution

---
+++ src/main/java/Distributor.java
@@ -0,0 +1,159 @@
+import com.opencsv.CSVWriter;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
+
+public class Distributor {
+  private BufferedWriter badStream;
+  private BufferedWriter logStream;
+  private CSVWriter badWriter;
+  private String dbPath;
+  private StringBuilder query;
+
+  /**
+   * Constructs a distributor to write records to <filename>.db
+   * and <filename>-bad.csv and <filename>.log
+   *
+   * @param filename Name of the files to output records to
+   */
+  Distributor(String filename) {
+    String csvPath = System.getProperty("user.dir") + System.getProperty("file.separator")  +
+        "output" + System.getProperty("file.separator") + filename + "-bad.csv";
+
+    dbPath = "jdbc:sqlite:" + System.getProperty("user.dir") + System.getProperty("file.separator") +
+        "output" + System.getProperty("file.separator") + filename + ".db";
+
+    String logPath = System.getProperty("user.dir") + System.getProperty("file.separator") +
+        "output" + System.getProperty("file.separator") + filename + ".log";
+
+    File csvOut = new File(csvPath);
+    File logOut = new File(logPath);
+    File dbFile = new File(System.getProperty("user.dir") + System.getProperty("file.separator") +
+        "output" + System.getProperty("file.separator") + filename + ".db");
+
+    if (csvOut.exists()) {
+      csvOut.delete();
+    }
+    if (logOut.exists()) {
+      logOut.delete();
+    }
+    if (dbFile.exists()) {
+      dbFile.delete();
+    }
+    try {
+      csvOut.createNewFile();
+      logOut.createNewFile();
+      badStream = new BufferedWriter(new FileWriter(csvOut));
+      logStream = new BufferedWriter(new FileWriter(logOut));
+      badWriter = new CSVWriter(badStream);
+      String[] line = {"A","B","C","D","E","F","G","H","I","J"};
+      badWriter.writeNext(line);
+      //badStream.write("A, B, C, D, E, F, G, H, I, J");
+      //badStream.newLine();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+      System.exit(1);
+    }
+    query = new StringBuilder("INSERT INTO Data VALUES");
+  }
+
+  /**
+   * Writes records to the correct file
+   *
+   * @param record Database values to be written to files
+   * @param isValid boolean that states whether a record is bad and must be
+   *                written to <filename>-bad.csv or is good and should be
+   *                written to <filename>.db
+   */
+  void write(String[] record, boolean isValid){
+    if (!isValid) {
+      badWriter.writeNext(record);
+      /**
+       String toWrite = "";
+       for (String entry: record) {
+         try {
+           badStream.write(entry);
+         } catch (IOException e) {
+           System.out.println(e.getMessage());
+           System.exit(1);
+         }
+        toWrite = toWrite + entry + ", ";
+       }
+       toWrite = toWrite.substring(0, toWrite.length() - 2);
+       try {
+        //badStream.write(toWrite);
+        badStream.newLine();
+       } catch (IOException e) {
+        System.out.println(e.getMessage());
+        System.exit(1);
+       }
+       */
+    } else {
+      StringBuilder statement = new StringBuilder(" (");
+      for (String entry : record) {
+        //need to escape the quote marks in these entries
+        entry = entry.replace("'", "''");
+        statement.append("'");
+        statement.append(entry);
+        statement.append("', ");
+      }
+      statement.setLength(statement.length() - 2);
+      statement.append("),");
+      query = query.append(statement.toString());
+      /**
+       String statement = " (";
+       for (String entry : record) {
+       //need to escape the quote marks in these entries
+       entry = entry.replace("'", "''");
+       statement = statement + "'" + entry + "', ";
+       }
+       statement = statement.substring(0, statement.length() - 2);
+       statement = statement + "),";
+       query = query + statement;
+       */
+    }
+  }
+
+  /**
+   * Writes the log data to its output file
+   *
+   * @param numRecords The number of records processed
+   * @param numValidRecords The number of valid records sent to the database
+   * @param numBadRecords The number of invalid records sent to csv
+   */
+  void log(int numRecords, int numValidRecords, int numBadRecords) {
+    try {
+      query.setLength(query.length() - 1);
+      query.append(";");
+      //the reason the table is created here is to be efficient
+      //I don't want to connect to the database twice
+      try {
+        Connection dbConn = DriverManager.getConnection(dbPath);
+        Statement stmt1 = dbConn.createStatement();
+        stmt1.execute("CREATE TABLE Data (A VARCHAR(255)" +
+            ", B VARCHAR(255), C VARCHAR(255), D VARCHAR(255), E VARCHAR(255)" +
+            ", F VARCHAR(255), G VARCHAR(255), H VARCHAR(255), I VARCHAR(255)" +
+            ", J VARCHAR(255));");
+        Statement stmt = dbConn.createStatement();
+        stmt.execute(query.toString());
+      } catch (SQLException e) {
+        System.out.println(e.getMessage());
+        System.exit(1);
+      }
+      logStream.write(numRecords + " Records Received");
+      logStream.newLine();
+      logStream.write(numValidRecords + " Records Successful");
+      logStream.newLine();
+      logStream.write(numBadRecords + " Records Failed");
+
+      badStream.close();
+      logStream.close();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+      System.exit(1);
+    }
+  }
+}
---
+++ src/main/java/Driver.java
@@ -0,0 +1,81 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * A driver class holding the main method for this application
+ *
+ * MountainParser takes a CSV file as input and transforms it into a database
+ * containing all valid records and a CSV file containing all invalid records
+ */
+public class Driver {
+  /**
+   * Main method for this application
+   *
+   * @param - args, The name of the file to convert in the 'input' directory
+   *          NOTE: DO NOT add the file extension
+   */
+  public static void main(String[] args) {
+
+    int numRecords = 0;
+    int numValidRecords = 0;
+    int numBadRecords = 0;
+
+    String filePath = System.getProperty("user.dir") + System.getProperty("file.separator") +
+        "input" + System.getProperty("file.separator") + args[0] + ".csv";
+    //CSVReader reader = new CSVReader();
+    Distributor distr = new Distributor(args[0]);
+    try {
+      CSVReader reader = new CSVReader(new FileReader(filePath));
+      //CSVParser parser = new CSVParser(args[0]);
+      boolean validLine = true;
+
+      //Get the columns from the first line of the file
+      //String[] values = parser.readLine();
+      String[] values = reader.readNext();
+      int numColumns = values.length;
+
+      //values = parser.readLine();
+      values = reader.readNext();
+      while (values != null) {
+        validLine = isValid(values, numColumns);
+        if (validLine) {
+          ++numValidRecords;
+        } else {
+          ++numBadRecords;
+        }
+        ++numRecords;
+
+        distr.write(values, validLine);
+        //values = parser.readLine();
+        values = reader.readNext();
+      }
+    } catch (CsvValidationException | IOException e) {
+      System.out.println(e.getMessage());
+      System.exit(1);
+    }
+    distr.log(numRecords, numValidRecords, numBadRecords);
+  }
+
+  /**
+   * Determines whether a record fulfills all required fields
+   *
+   * @param record The records to be checked
+   * @return true if the records contain values in all fields, false otherwises
+   */
+  static boolean isValid(String[] record, int numColumns) {
+    if (record.length != numColumns) {
+      return false;
+    }
+    for(String item: record) {
+      if (item.isEmpty()) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
